0596b282f0855f301828aa4299ede27b27087112
Júlia <juliarodrigues.mjr@gmail.com>
2022-10-01 19:15:40 -0300

    inclusão dessa merda não vai

--- projeto.video.git/src/main/java/projeto/video/git/HelloWorld.java
+++ projeto.video.git/src/main/java/projeto/video/git/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 				System.out.println("Hello World");
 				System.out.println("Git hub");
 				System.out.println("teste");
+				System.out.println("essa bosta não vai");
 			}
 
 		
